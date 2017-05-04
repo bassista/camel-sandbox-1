dbe4930cf5577434f2c355632e84af5bb7d87979
lburgazzoli <lburgazzoli@gmail.com>
2017-05-04 12:31:59 +0200

    Add camel-maven-plugin

--- src/main/java/com/github/lburgazzoli/Application.java
+++ src/main/java/com/github/lburgazzoli/Application.java
@@ -6,7 +6,6 @@ import org.apache.camel.spring.Main;
 public class Application {
     public static void main(String... args) throws Exception {
         Main main = new Main();
-        main.setApplicationContextUri("classpath:camel.xml");
         main.run(args);
     }
 }
